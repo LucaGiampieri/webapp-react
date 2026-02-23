5392fae2785ae8860cd567557cfeb25f4682e148
Luca Giampieri <luca.giampieri95@gmail.com>
2026-02-23 19:21:51 +0100

    Aggiunta CardReview

--- src/components/CardReview.jsx
+++ src/components/CardReview.jsx
@@ -1,16 +1,13 @@
 function CardReview() {
 
     return (
-        <main>
-            <div className="home-title-container">
-                <h1 className="home-title">Bool Movie</h1>
-                <h2 className="home-subtitle">The boolean nerd movie community</h2>
+        <div className="card-review-container">
+            <div className="card-review-name-vote-container">
+                <h4 className="card-review-name">By Mario Rossi</h4>
+                <h4 className="card-review-vote">Vote:10</h4>
             </div>
-            <div className="home-card-container">
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim adipisci, expedita non, quo corrupti voluptatum vero aliquam ullam magni nobis voluptas ad accusamus assumenda sapiente inventore mollitia? Eius, praesentium soluta?</p>
-            </div>
-        </main>
+            <p className="card-review-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, iure dolorum provident porro possimus reiciendis nemo excepturi aspernatur dolore! Quaerat reprehenderit eveniet consequuntur voluptatem omnis harum, inventore commodi deleniti obcaecati?</p>
+        </div>
     )
 }
 
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -7,27 +7,31 @@ import CardReview from "../components/CardReview"
 function MoviePage() {
 
     return (
-        <main>
-            <div className="movie-container">
-                <div className="movie-img-container">
-                    <img className="movie-img" src="https://pad.mymovies.it/filmclub/2002/06/038/locandina.jpg" alt="Metropolis" />
-                </div>
-                <div className="movie-description-container">
-                    <h1 className="movie-title">Metropolis</h1>
-                    <h2 className="movie-director">Directed by Fritz Lang</h2>
-                    <p className="movie-description">
-                        Set in a futuristic, dystopian city in the year 2026, Metropolis depicts a sharply stratified society. The wealthy elite live in luxury in skyscrapers and "Pleasure Gardens" above ground, while the working class toils in an underground, dark industrial city, operating the machinery that powers the upper world.
-                    </p>
-                    <div className="movie-back-btn-container">
-                        <Link className="movie-back-btn" to="/">Back to home</Link>
+        <>
+            <main>
+                <div className="movie-container">
+                    <div className="movie-img-container">
+                        <img className="movie-img" src="https://pad.mymovies.it/filmclub/2002/06/038/locandina.jpg" alt="Metropolis" />
+                    </div>
+                    <div className="movie-description-container">
+                        <h1 className="movie-title">Metropolis</h1>
+                        <h2 className="movie-director">Directed by Fritz Lang</h2>
+                        <p className="movie-description">
+                            Set in a futuristic, dystopian city in the year 2026, Metropolis depicts a sharply stratified society. The wealthy elite live in luxury in skyscrapers and "Pleasure Gardens" above ground, while the working class toils in an underground, dark industrial city, operating the machinery that powers the upper world.
+                        </p>
+                        <div className="movie-back-btn-container">
+                            <Link className="movie-back-btn" to="/">Back to home</Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <CardReview />
-
-
-
-        </main>
+                <div className="movie-review-container">
+                    <h3 className="movie-title-review">Our community reviews</h3>
+                    <CardReview />
+                    <CardReview />
+                    <CardReview />
+                </div>
+            </main>
+        </>
     )
 }
 
