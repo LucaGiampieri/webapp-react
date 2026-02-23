4991739c90a5f06b249fb07e39ea483abbe9e2d9
Luca Giampieri <luca.giampieri95@gmail.com>
2026-02-23 14:53:38 +0100

    struttura HomePage (senza cards)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,16 @@
 function HomePage() {
 
     return (
-        <h2>HomePage</h2>
+        <main>
+            <div className="home-title-container">
+                <h1 className="home-title">Bool Movie</h1>
+                <h2 className="home-subtitle">The boolean nerd movie community</h2>
+            </div>
+            <div className="home-card-container">
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim adipisci, expedita non, quo corrupti voluptatum vero aliquam ullam magni nobis voluptas ad accusamus assumenda sapiente inventore mollitia? Eius, praesentium soluta?</p>
+            </div>
+        </main>
     )
 }
 
