a95ebe2e382ebcbc356ee2d127f61691bd93cf37
Luca Giampieri <luca.giampieri95@gmail.com>
2026-02-26 14:05:59 +0100

    Creazione componente di Loader e import nel Layot (per ora sempre fisso)

--- src/layouts/DefaultLayouts.jsx
+++ src/layouts/DefaultLayouts.jsx
@@ -4,11 +4,15 @@ import { Outlet } from "react-router-dom";
 //import di MainHeader
 import MainHeader from "../components/MainHeader";
 
+//import del Loader
+import Loader from "../components/Loader";
+
 const DefaultLayout = () => {
     return (
         <>
             <MainHeader />
             <Outlet />
+            <Loader />
         </>
     )
 }
